440da5e11637ab1695e785fc78dfa8eeb58ddccc
MaeeshaMeem34 <56674927+MaeeshaMeem34@users.noreply.github.com>
2021-01-06 10:42:53 +0600

    Adding other testfiles

--- ITesting.cs
+++ ITesting.cs
@@ -1,44 +1,77 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Firefox;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Selenium_demo
+{
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using System;
 
-namespace Selenium_demo
-{ 
+  
+    internal class ITesting
+    {
+        
+        internal IWebDriver driver;
 
-        class ITesting
+        /// <summary>
+        /// The startBrowser.
+        /// </summary>
+        [SetUp]
+        public void startBrowser()
         {
-            IWebDriver driver;
-            [SetUp]
-            public void startBrowser()
-            {
-                driver = new ChromeDriver();
-            }
-            [Test]
-            public void test()
-            {
-                driver.Url = "https://elp.duetbd.org/login/index.php";
-                IWebElement element = driver.FindElement(By.Name("username"));
-                element.SendKeys("170042034");
-                IWebElement password = driver.FindElement(By.Name("password"));
-                password.SendKeys("Abc.1234");
-                driver.FindElement(By.Id("loginbtn")).Click();
-             
-              
-            }
-            //[TearDown]
-            public void closeBrowser()
-            {
-            
-                driver.Close();
-            }
+            driver = new ChromeDriver();
+        }
+
+      
+        [Test]
+        public void testLogin()
+        {
+            driver.Url = "https://elp.duetbd.org/login/index.php";
+            IWebElement element = driver.FindElement(By.Name("username"));
+            element.SendKeys("170042034");
+            IWebElement password = driver.FindElement(By.Name("password"));
+            password.SendKeys("Abc.1234");
+            driver.FindElement(By.Id("loginbtn")).Click();
+        }
+
+
+
+    
+
+        [Test]
+        public void testNavigation()
+        {
+            driver.Url = "https://elp.duetbd.org/login/index.php";
+            driver.Navigate().Refresh();
+           
+        }
+
+
+
+
+
+        [Test]
+        public void testTagName()
+        {
+            driver.Url = "https://elp.duetbd.org/login/index.php";
+            IWebElement element =driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[1]/form/button"));
+            string tag = element.TagName;
+            Console.WriteLine(tag);
+        }
+
+
+        [Test]
+        public void testTextName()
+        {
+            driver.Url = "https://elp.duetbd.org/login/index.php";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[1]/form/button"));
+            string text = element.Text;
+            Console.WriteLine(text);
+        }
+
+
+        public void closeBrowser()
+        {
+
+            driver.Close();
         }
     }
-
-
+}
---
+++ ITesting1.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_demo
+{
+    class ITesting1
+    { 
+     internal IWebDriver driver;
+
+    /// <summary>
+    /// The startBrowser.
+    /// </summary>
+    [SetUp]
+    public void startBrowser()
+    {
+        driver = new ChromeDriver();
+    }
+
+
+    [Test]
+    public void testOpenBrowser()
+    {
+        driver.Url = "http://automationpractice.com/index.php?";
+       
+    }
+
+
+
+
+
+    [Test]
+    public void testRefresh()
+    {
+        driver.Url = "http://automationpractice.com/index.php?";
+        driver.Navigate().Refresh();
+
+    }
+
+
+
+
+
+    [Test]
+    public void testTagName()
+    {
+        driver.Url = "http://automationpractice.com/index.php?";
+        IWebElement element = driver.FindElement(By.XPath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/input[4]"));
+        string tag = element.TagName;
+        Console.WriteLine(tag);
+    }
+
+
+    [Test]
+    public void testSendText()
+    {
+        driver.Url = "http://automationpractice.com/index.php?";
+        IWebElement element = driver.FindElement(By.Name("search_query"));
+        element.SendKeys("shirt");
+        driver.FindElement(By.Name("submit_search")).Click();
+        }
+
+    [Test]
+    public void testElementColor()
+        {
+            driver.Url = "http://automationpractice.com/index.php?";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div/div[1]/header/div[3]/div/div/div[3]/div/a"));
+            string color = element.GetCssValue("background");
+            Console.WriteLine(color);
+        }
+
+    [Test]
+    public void testFontWeight()
+        {
+            driver.Url = "http://automationpractice.com/index.php?";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div/div[1]/header/div[3]/div/div/div[3]/div/a"));
+            string weight = element.GetCssValue("font-weight");
+            Console.WriteLine(weight);
+        }
+
+
+
+
+        public void closeBrowser()
+    {
+
+        driver.Close();
+    }
+    
+    }
+}
